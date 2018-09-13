e1e57bf2d62960787834410474f0127ee744b24c
Shaun Cullen <shaun.cullen@canada.ca>
2018-09-13 13:19:57 -0400

    leaflet map added

--- src/renderer/renderer.tsx
+++ src/renderer/renderer.tsx
@@ -4,9 +4,15 @@ import * as ReactDOM from 'react-dom';
 // Import the styles here to process them with webpack
 import './style.scss';
 
+import MapContainer from './components/map-container';
+
 ReactDOM.render(
-  <div>
+  <div className="main-content">
     <h4>Welcome to React, Typescript, and Electron</h4>
+    {/* <div id="mapid" /> */}
+    <MapContainer />
+    <div>Sup</div>
+    <h3>Hello There!</h3>
   </div>,
   document.getElementById('app')
 );
