282d1b35d9aa77bf943970626798bbfdfc455b30
Shaun Cullen <shaun.cullen@canada.ca>
2018-09-13 11:42:29 -0400

    initial commit

---
+++ src/declarations.d.ts
@@ -0,0 +1,20 @@
+// Put all of your custom type information for your 3rd part modules here
+declare module '*.svg' {
+  const value: any;
+  export = value;
+}
+
+declare module '*.png' {
+  const value: any;
+  export = value;
+}
+
+declare module '*.ico' {
+  const value: any;
+  export = value;
+}
+
+declare module '*.icns' {
+  const value: any;
+  export = value;
+}
---
+++ src/renderer/renderer.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+// Import the styles here to process them with webpack
+import './style.scss';
+
+ReactDOM.render(
+  <div>
+    <h4>Welcome to React, Typescript, and Electron</h4>
+  </div>,
+  document.getElementById('app')
+);
